3d935dd9d995540864f35a40bafb7cb99733d963
torant <sergebrunet23@gmail>
2020-10-05 21:41:57 +0000

    Exo Module 5

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
 
 namespace WishList.Controllers
 {
---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _context.Items;
+            return View("Index", model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Models.Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var itemId = _context.Items.Find(id);
+            _context.Items.Remove(itemId);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
